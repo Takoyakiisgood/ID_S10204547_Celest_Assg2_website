07dece2cceef7c46b07b5eb91026bed8af90359f
Takoyakiisgood <s10204547@connect.np.edu.sg>
2021-01-08 17:30:06 +0800

    Create generator button

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 var settings = {
-    "url": "https://api.documenu.com/v2/restaurants/search/fields",
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-      "X-API-KEY": "d78d7d9478a5031ba8a0803c184e8840",
-      "Cookie": "__cfduid=daf1bf80f2e6814131aa5307fc10fac1c1609050615"
-    },
-  };
+  "url": "https://www.themealdb.com/api/json/v1/1/random.php",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Cookie": "__cfduid=d00e058a37eb5cdfd93b44f0831699dc41610094802"
+  },
+};
 
 $(document).ready(function () {
 
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-      });
-      
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+    
 }
